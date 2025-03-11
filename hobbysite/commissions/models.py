755fa22655b39a97a752cde41ff7c5a85945a9bc
Lloyd Leano <leanolloyd@gmail.com>
2025-03-11 18:48:45 +0800

    [models.py] Implemented Commission and Comment models.

--- hobbysite/commissions/models.py
+++ hobbysite/commissions/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Commission(models.Model):
+    title = models.Charfield(max_length=255)
+    description = models.TextField()
+    people_required = models.PositiveIntegerField()
+    created_on = models.DateTimeField(auto_now=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+class Comment(models.Model):
+    commission = models.ForeignKey(Commission, on_delete=models.CASCADE)
+    entry = models.TextField()
+    created_on = models.DateTimeField(auto_now=True)
+    updated_on = models.DateTimeField(auto_now=True)
