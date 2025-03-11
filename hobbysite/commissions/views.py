d89cc60e54ff9c213154c246af1bb1ff8b68e419
Lloyd Leano <leanolloyd@gmail.com>
2025-03-11 18:58:00 +0800

    [views.py] Imported necessary models and django commands. Created temporary def functions for commission_list() and comment_list()

--- hobbysite/commissions/views.py
+++ hobbysite/commissions/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from .models import Commission, Comment
 
 # Create your views here.
+def commission_list(request):
+    commissions = Commission.objects.order_by("created_on")
+    #return
+
+def comment_list(request):
+    comments = Comment.objects.objects.order_by("-created_on")
+    #return
