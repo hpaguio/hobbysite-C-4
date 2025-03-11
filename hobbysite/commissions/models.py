925dead070fd91c30adbbe2490ba125b06861904
Lloyd Leano <leanolloyd@gmail.com>
2025-03-11 19:46:37 +0800

    [models.py][commissions_detail.html] Fixed typos

--- hobbysite/commissions/models.py
+++ hobbysite/commissions/models.py
@@ -2,14 +2,14 @@ from django.db import models
 
 # Create your models here.
 class Commission(models.Model):
-    title = models.Charfield(max_length=255)
+    title = models.CharField(max_length=255)
     description = models.TextField()
     people_required = models.PositiveIntegerField()
-    created_on = models.DateTimeField(auto_now=True)
+    created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
 
 class Comment(models.Model):
     commission = models.ForeignKey(Commission, on_delete=models.CASCADE)
     entry = models.TextField()
-    created_on = models.DateTimeField(auto_now=True)
+    created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
