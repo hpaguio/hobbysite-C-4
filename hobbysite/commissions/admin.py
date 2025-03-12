757aae2efe3bba087ab132d403d990de7ad8f920
Lloyd Leano <leanolloyd@gmail.com>
2025-03-12 18:17:01 +0800

    [admin.py] Implemented admin.py and imported the models.

--- hobbysite/commissions/admin.py
+++ hobbysite/commissions/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Commission, Comment
 
 # Register your models here.
+
+class CommissionAdmin(admin.ModelAdmin):
+    model = Commission
+    
+    list_display = ('title', 'created_on', 'updated_on')
+    
+class CommentAdmin(admin.ModelAdmin):
+    model = Comment
+    
+    list_display = ('commission', 'created_on', 'updated_on',)
+    
+admin.site.register(Commission, CommissionAdmin)
+admin.site.register(Comment, CommentAdmin)
