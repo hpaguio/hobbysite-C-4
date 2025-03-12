1426bac67c6fe52506410b3c83eb2af34445439c
Lloyd Leano <leanolloyd@gmail.com>
2025-03-12 17:20:06 +0800

    [views.py] Edited the def functions, changed name to def commission_details(), and finished return statements.

--- hobbysite/commissions/views.py
+++ hobbysite/commissions/views.py
@@ -6,8 +6,11 @@ from .models import Commission, Comment
 # Create your views here.
 def commission_list(request):
     commissions = Commission.objects.order_by("created_on")
-    #return
+    
+    return render(request, 'commissions_list', {'commission':commissions})
 
-def comment_list(request):
+def commission_details(request, param):
+    commissions = Commission.objects.get(id=param)
     comments = Comment.objects.objects.order_by("-created_on")
-    #return
+
+    return render(request, 'commissions_detail.html', {'commission':commissions, 'comments':comments})
