4cf59cb13d91758a8323a821efe6aac23943857f
hpaguio <hendrickpaguio@gmail.com>
2025-03-13 21:02:53 +0800

    [views.py] - Added the function blog_categories to list down all article categories in ascending order and also added the function blog_article to list the articles by date created in descending order

--- hobbysite/blog/views.py
+++ hobbysite/blog/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from .models import ArticleCategory, Article
 
 # Create your views here.
+def blog_categories(request):
+    article_categories = ArticleCategory.objects.order_by("name")
+    
+    return render(request, 'article_list.html', {'article_category':article_categories})
+
+def blog_article(request, param):
+    article_categories = ArticleCategory.objects.get(id=param)
+    articles = Article.objects.filter(article=article_categories).order_by("-created_on")
+
+    return render(request, 'article.html', {'article_category':article_categories, 'article':articles})
