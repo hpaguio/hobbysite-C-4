0cbecb15b5adfdb435adc2955c71fd5caaeedd61
Samuel Wood <woodsamueldrury@gmail.com>
2025-03-13 22:47:22 +0800

    [admin.py] Implemented admin.

--- hobbysite/merchstore/admin.py
+++ hobbysite/merchstore/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import ProductType, Product
 
 # Register your models here.
+
+class ProductTypeAdmin(admin.ModelAdmin):
+	model = ProductTypeAdmin
+
+	list_display = ('name', 'description')
+
+class ProductAdmin(admin.ModelAdmin):
+	model = Product
+
+	list_display = ('name', 'product_type', 'description', 'price')
+
+admin.site.register(ProductType, ProductTypeAdmin)
+admin.site.register(Product, ProductAdmin)
