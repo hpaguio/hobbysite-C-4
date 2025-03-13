28035cfb200be3a6c4f2ec9c61acefc86d9066ed
hpaguio <hendrickpaguio@gmail.com>
2025-03-13 23:09:04 +0800

    [blog] - Final commit to make sure everything is committed and pushed properly.

--- hobbysite/blog/views.py
+++ hobbysite/blog/views.py
@@ -8,7 +8,7 @@ def blog_categories(request):
     article_categories = ArticleCategory.objects.order_by("name")
     
     return render(request, 'article_list.html', {'article_category':article_categories})
-
+ 
 def blog_article(request, param):
     article_categories = ArticleCategory.objects.get(id=param)
     articles = Article.objects.filter(category=article_categories).order_by("-created_on")
