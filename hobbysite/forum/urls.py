d6254fa10a4f4ad9b973e3e5ed9e230074450619
Lawrence Paul Cado Marino <lawrencemarino18@gmail.com>
2025-03-13 22:51:39 +0800

    [forum] edited URL Pathing

--- hobbysite/forum/urls.py
+++ hobbysite/forum/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from .views import PostListView, PostDetailView
 
-app_name = 'forum'
+app_name = 'forum'  # This enables namespacing
 
 urlpatterns = [
-    path('', PostListView.as_view(), name='forum-home'),
-    path('<int:pk>/', PostDetailView.as_view(), name='thread-detail'),
+    path('', PostListView.as_view(), name='thread-list'),  # Keep this
+    path('<int:pk>/', PostDetailView.as_view(), name='thread-detail'),  # Rename this
 ]
