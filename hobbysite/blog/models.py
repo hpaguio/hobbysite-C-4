93bda2b406f8e2f9e3e649e165f3fe0eead48810
hpaguio <hendrickpaguio@gmail.com>
2025-03-13 21:58:30 +0800

    [models.py] - Fixed typos and correctly implemented SET_NULL on delete

--- hobbysite/blog/models.py
+++ hobbysite/blog/models.py
@@ -2,13 +2,12 @@ from django.db import models
 
 # Create your models here.
 class ArticleCategory(models.Model):
-    name = models.CharField(max_length = 255)
+    name = models.CharField(max_length=255)
     description = models.TextField()
 
-
 class Article(models.Model):
-    title = models.CharField(max_length = 255)
-    category = models.foreginKey(ArticleCategory, on_delete = models.CASCADE)
+    title = models.CharField(max_length=255)
+    category = models.ForeignKey(ArticleCategory, on_delete=models.SET_NULL, null=True)
     entry = models.TextField()
-    created_on = models.DateTimeField(auto_now_add = True)
-    updated_on = models.DateTimeField(auto_now = True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
