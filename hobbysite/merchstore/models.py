4873d9c85ffddba0910aa5e65317f7ce98206abc
Samuel Wood <woodsamueldrury@gmail.com>
2025-03-13 21:54:42 +0800

    [models.py] Implemented ProductType and Product models.

--- hobbysite/merchstore/models.py
+++ hobbysite/merchstore/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class ProductType(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    class Meta:
+		ordering = ['name']
+
+class Product(models.Model):
+	name = models.CharField(max_length=255)
+    product_type = models.ForeignKey(ProductType, on_delete=models.SET_NULL)
+    description = models.TextField()
+    price = models.DecimalField(decimal_places=2)
+    class Meta:
+		ordering = ['name']
