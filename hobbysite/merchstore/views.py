aaa883b5362b04d55954922c965c32161db9374a
Samuel Wood <woodsamueldrury@gmail.com>
2025-03-13 22:04:19 +0800

    [views.py] Imported necessary models and Django commands. Created placeholder views for list and detail.

--- hobbysite/merchstore/views.py
+++ hobbysite/merchstore/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from django.template import loader
+from .models import ProductType, Product
+ 
 # Create your views here.
+ 
+
+def list(request):
+    product_type = ProductType.objects.order_by("name")
+    #return
+
+def detail(request):
+    product = Product.objects.order_by("name")
+    #return
