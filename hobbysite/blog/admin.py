e9f9036193701643d48fe89baa31f0e00f1a2885
hpaguio <hendrickpaguio@gmail.com>
2025-03-13 22:24:01 +0800

    [admin.py] - Added the admin panel for the blog app

--- hobbysite/blog/admin.py
+++ hobbysite/blog/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Article, ArticleCategory
 
 # Register your models here.
+
+class ArticleAdmin(admin.ModelAdmin):
+    model = Article
+    
+    list_display = ('title', 'created_on', 'updated_on',)
+    
+class CategoriesAdmin(admin.ModelAdmin):
+    model = ArticleCategory
+    
+    list_display = ('title', 'description',)
+    
+admin.site.register(Article, ArticleAdmin)
+admin.site.register(ArticleCategory, CategoriesAdmin)
