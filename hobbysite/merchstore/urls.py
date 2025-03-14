3af0b4cb0749398c7a1d5327e8efd14714313a82
Lawrence Paul Cado Marino <lawrencemarino18@gmail.com>
2025-03-14 19:37:27 +0800

    [merchstore] edited URL pathing

--- hobbysite/merchstore/urls.py
+++ hobbysite/merchstore/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
-from . import views
 from .views import merchstore_list, merchstore_detail
 
 app_name = "merchstore"
 
 urlpatterns = [
     path("", merchstore_list, name="merchstore_home"),
-    path('merchstore/list', merchstore_list, name="merchstore_list"),
-    path('merchstore/item/<int:param>', merchstore_detail, name="merchstore_detail"),
-    ]
+    path("list/", merchstore_list, name="merchstore_list"),
+    path("item/<int:param>/", merchstore_detail, name="merchstore_detail"),
+]
